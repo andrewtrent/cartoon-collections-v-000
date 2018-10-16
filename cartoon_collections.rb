1884562c154744ee27fe27bbbf695bc486f830d6
Andrew Trent <andrewtrentdesign@gmail.com>
2018-10-16 22:23:47 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,11 +7,7 @@ def summon_captain_planet(elements)
 end
 
 def long_planeteer_calls(calls)
-  if calls.any? { |call| call.length > 3 }
-    true
-  else
-    false
-  end
+  calls.any? { |call| call.length > 3 }
 end
 
 def find_the_cheese# code an argument here
